4d9a2cc6cc6379587cc0761fa06f4048680e439f
saikiran12040 <saikirangen@gmail.com>
2023-06-12 16:16:12 +0530

    added file

---
+++ server.js
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 8080
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(PORT, () => {
+  console.log(`port server running on ${PORT}`)
+})
